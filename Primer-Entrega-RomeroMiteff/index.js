89cca3117d43f6393fb265ed9f5c4bcbbb961deb
FedericoRomeroMiteff <federomeromiteff@hotmail.com>
2024-02-06 20:03:49 -0300

    1

---
+++ Primer-Entrega-RomeroMiteff/index.js
@@ -0,0 +1,58 @@
+// let opt = 1;
+
+// while (opt != "esc") {
+//   switch (opt) {
+//     case "esc":
+//       console.log("salir");
+//       break;
+//     case "1":
+//       console.log("1");
+//       break;
+//     case "2":
+//       console.log("2");
+//       break;
+//     case "3":
+//       console.log("3");
+//       break;
+//     case "4":
+//       console.log("4");
+//       break;
+//     default:
+//       console.log("defa");
+//       break;
+//   }
+//   opt = prompt("1 a 4 o esc");
+// }
+
+// while (sorteo !== "ESC") {
+//   switch (sorteo) {
+//     case "ESC":
+//       console.log("Seguí participando");
+//       alert("Seguí participando");
+//     case "1":
+//       console.log("Excelente ¡ganaste!");
+//       alert("Excelente ¡ganaste!");
+//       break;
+//     case "2":
+//       console.log("Buena suerte la próxima");
+//       alert("Buena suerte la próxima");
+//       break;
+//     case "3":
+//       console.log("¡Te esperamos para que busques tu regalo!");
+//       alert("¡Te esperamos para que busques tu regalo!");
+//       break;
+//     default:
+//       console.log("Carga la opción correcta y proba suerte");
+//       alert("Carga la opción correcta y proba suerte");
+//   }
+//   sorteo = parseInt(prompt("Ingrese un número del 1 al 3 o ESC para terminar"));
+// }
+
+const nombre = "Federico";
+const apellido = "Romero Miteff";
+
+const saludo = nombre + " " + apellido + " " + apellido;
+
+console.log(saludo);
+
+alert(saludo);
