8091c811f8790172afddeefeb83832085fd8af41
FedericoRomeroMiteff <federomeromiteff@hotmail.com>
2024-02-27 20:05:06 -0300

    Update index.js

--- Segunda-Entrega-RomeroMiteff/index.js
+++ Segunda-Entrega-RomeroMiteff/index.js
@@ -1,94 +1,83 @@
 function saludo() {
   return prompt("Ingrese nombre y apellido");
 }
-// CONVERSIÓN PESOS A DÓLARES
+
 {
   let nombre = saludo();
-  alert("Bienvenido " + nombre);
-
-  let valorEnPesos = parseInt(
-    prompt("Ingresar el valor en pesos a convertir ($) sin puntos.")
-  );
-  let valorEnDolares = conversionPesoDolar;
-
-  function conversionPesoDolar() {
-    return (valorEnDolares = valorEnPesos / 1175);
-  }
-
-  conversionPesoDolar();
-
   alert(
-    "El valor en pesos es: $" +
-      valorEnPesos +
-      "." +
-      " El valor en dólares es: $" +
-      valorEnDolares +
-      "."
+    "Bienvenido al simulador interactivo de cotización de divisas " + nombre
   );
 }
-// CONVERSIÓN DÓLARES A PESOS
-{
-  let valorEnDolares = parseInt(
-    prompt("Ingresar el valor en dólares a convertir (USD) sin puntos.")
-  );
-  let valorEnPesos = conversionDolarPeso;
 
-  function conversionDolarPeso() {
-    return (valorEnPesos = valorEnDolares * 1150);
+let valorEnPesos = prompt(
+  "Ingresar el valor en pesos a convertir ($) sin puntos. \nESC para salir."
+);
+
+class divisa {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
   }
-
-  conversionDolarPeso();
-
-  alert(
-    " El valor en dólares es: $" +
-      valorEnDolares +
-      "." +
-      "El valor en pesos es: $" +
-      valorEnPesos +
-      "."
-  );
 }
-// CONVERSIÓN PESOS A EUROS
-{
-  let valorEnPesos = parseInt(
-    prompt("Ingresar el valor en pesos a convertir ($) sin puntos.")
-  );
-  let valorEnEuros = conversionPesoEuro;
 
-  function conversionPesoEuro() {
-    return (valorEnEuros = valorEnPesos / 1245);
-  }
+const dolar = new divisa("dolares", 1085);
+const euro = new divisa("euros", 1172);
+const historial = [];
 
-  conversionPesoEuro();
-
+function conversiondivisa(pesosArgentinos, valordivisa, divisa) {
   alert(
-    "El valor en pesos es: $" +
+    "$" +
       valorEnPesos +
-      "." +
-      " El valor en euros es: $" +
-      valorEnEuros +
-      "."
+      " equivalen a " +
+      (pesosArgentinos / valordivisa).toFixed(2) +
+      " " +
+      divisa
   );
+  const operacionAGuardar = {
+    valorEnPesos: valorEnPesos,
+    resultado: (pesosArgentinos / valordivisa).toFixed(2),
+    divisa: divisa,
+  };
+
+  historial.push(operacionAGuardar);
 }
-// CONVERSIÓN EUROS A PESOS
-{
-  let valorEnEuro = parseInt(
-    prompt("Ingresar el valor en euros a convertir (EUR) sin puntos.")
-  );
-  let valorEnPesos = conversionEuroPeso;
 
-  function conversionEuroPeso() {
-    return (valorEnPesos = valorEnEuro * 1212);
+while (
+  valorEnPesos.toLowerCase() != "esc" &&
+  valorEnPesos.toLowerCase() != "historial"
+) {
+  let divisaElegida = parseInt(
+    prompt(
+      "Ingrese divisa en la que quiere hacer la conversión: \n 1 - Dólares \n 2 - Euros "
+    )
+  );
+
+  switch (divisaElegida) {
+    case 1:
+      conversiondivisa(valorEnPesos, dolar.precio, dolar.nombre);
+      break;
+
+    case 2:
+      conversiondivisa(valorEnPesos, euro.precio, euro.nombre);
+      break;
+
+    default:
+      alert("divisa ingresada incorrecta. Volver a intentar.");
   }
 
-  conversionEuroPeso();
-
-  alert(
-    " El valor en euros es: $" +
-      valorEnEuro +
-      "." +
-      "El valor en pesos es: $" +
-      valorEnPesos +
-      "."
+  valorEnPesos = prompt(
+    "Ingrese el valor en pesos a convertir ($) sin puntos o escriba HISTORIAL para ver operaciones anteriores.\n ESC para salir."
   );
+
+  if (valorEnPesos.toLowerCase() === "historial") {
+    let mensaje = "Ultimas operaciones realizadas: \n";
+
+    historial.forEach((operacion) => {
+      mensaje =
+        mensaje +
+        `El valor de ${operacion.valorEnPesos} pesos argentinos es equivalente a ${operacion.resultado} ${operacion.divisa}.\n`;
+    });
+
+    alert(mensaje);
+  }
 }
